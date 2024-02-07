86c9b1c78486662904fbf20f43a06ad355cdd3e8
ITsPorky <itsporky16@gmail.com>
2024-02-07 16:48:20 +1000

    Added localStorage support for dark mode, so browser remembers your choice after reload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from "react-router-dom";
 import './App.css';
 import Header from './components/common/Header';
 import Demo from './components/pages/Demo';
@@ -6,12 +7,24 @@ import Home from './components/pages/Home';
 
 
 function App() {
-  const [darkMode, setDarkMode] = useState(0);
+  const [darkMode, setDarkMode] = useState(
+    JSON.parse(localStorage.getItem('dark-mode')) || false
+  );
+
+  // Run on Component load
+  useEffect(() => {
+    applyDarkMode(darkMode);
+  })
 
   const toggleDarkMode = () => {
+    localStorage.setItem('dark-mode', JSON.stringify(!darkMode));
     setDarkMode(!darkMode);
+    applyDarkMode(darkMode);
+  };
+
+  const applyDarkMode = (bool) => {
     // Set Body element colours
-    if(!darkMode) {
+    if(!bool) {
       document.body.classList.add('dark');
     } else {
       document.body.classList.remove('dark');
@@ -20,16 +33,21 @@ function App() {
 
   return (
     <div className={`App`}>
-      <Header darkModeCallback={toggleDarkMode}>
-        <a href='/'>Home</a>
-        <a href='/'>About</a>
-        <a href='/'>Services</a>
-        <a href='/'>Contact</a>
-      </Header>
-      <main>
-        <Home />
-        <Demo />
-      </main>
+      <Router>
+        <Header darkModeCallback={toggleDarkMode}>
+          <Link to='/'>Home</Link>
+          <Link to='/demo'>Demo</Link>
+          <Link to='#'>Services</Link>
+          <Link to='#'>Contact</Link>
+        </Header>
+        <main>
+          <Routes>
+            <Route path='/' Component={Home}/>
+            <Route path='/demo' Component={Demo}/>
+            <Route path='/redirect' element={ <Navigate to='/not-found' /> } />
+          </Routes>
+        </main>
+      </Router>
     </div>
   );
 }
--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,6 +2,7 @@ import React, { Children, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faXmark, faCircleHalfStroke } from "@fortawesome/free-solid-svg-icons";
 import Logo from '../../logo.svg';
+import Menu from "./Menu";
 
 const Header = ({ children, darkModeCallback }) => {
   const [isDrawerOpen, setDrawerOpen] = useState(false);
@@ -20,23 +21,24 @@ const Header = ({ children, darkModeCallback }) => {
     <header className="py-1.5">
       {/* Nav Menu */}
       <nav className="nav-menu flex w-full h-auto justify-between items-center">
-        <div>
+        <div className="flex flex-row items-center md:w-1/3">
           <a href="/">
-            <img src={Logo} alt="App Logo" className="logo" Style="max-width: 80px" />
+            <img src={Logo} alt="App Logo" className="logo" />
           </a>
+          <p>React Template</p>
         </div>
-        <ul className="nav-menu-items hidden md:flex flex-row px-5">
+        <ul className="nav-menu-items hidden md:flex flex-row justify-center md:w-2/3 px-5">
           {Children.map(children, child => {
             return <li className="px-5">{child}</li>;
           })}
         </ul>
-        <div className="flex flex-row">
-          <div className="nav-darkmode-toggle-container block px-5">
+        <div className="flex flex-row justify-end md:w-1/3">
+          <div className="nav-darkmode-toggle-container block px-2">
             <div className="nav-darkmode-toggle p-2 cursor-pointer" onClick={darkModeCallback}>
-              <FontAwesomeIcon icon={faCircleHalfStroke} fontSize={18}/>
+              <FontAwesomeIcon icon={faCircleHalfStroke} fontSize={20}/>
             </div>
           </div>
-          <div className="nav-menu-toggle-container block md:hidden px-5">
+          <div className="nav-menu-toggle-container block md:hidden px-2">
             <div className="nav-menu-toggle p-2 cursor-pointer" onClick={toggleDrawer}>
               <FontAwesomeIcon icon={faBars} fontSize={20}/>
             </div>
@@ -50,7 +52,7 @@ const Header = ({ children, darkModeCallback }) => {
         } md:hidden`}
       >
         <div className="nav-menu-toggle-container mobile">
-          <div className="nav-menu-toggle mobile" onClick={toggleDrawer}>
+          <div className="nav-menu-toggle mobile py-1.5" onClick={toggleDrawer}>
             <FontAwesomeIcon icon={faXmark} fontSize={20} />
           </div>
         </div>
