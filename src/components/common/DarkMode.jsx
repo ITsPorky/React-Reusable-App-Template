d81486c0f6043744c98b97317263efb8eb5f3bd8
ITsPorky <itsporky16@gmail.com>
2024-02-09 14:18:54 +1000

    Moved DarkMode to be a standalone Component

---
+++ src/components/common/DarkMode.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+
+const DarkMode = ({ icon, text }) => {
+    const [darkMode, setDarkMode] = useState(
+        JSON.parse(localStorage.getItem('dark-mode')) || false
+    );
+
+    // Run on Component load
+    useEffect(() => {
+        applyDarkMode(darkMode);
+    })
+
+    const toggleDarkMode = () => {
+    localStorage.setItem('dark-mode', JSON.stringify(!darkMode));
+        setDarkMode(!darkMode);
+        applyDarkMode(darkMode);
+    };
+
+    const applyDarkMode = (bool) => {
+    // Set Body element colours
+    if(!bool) {
+        document.body.classList.add('dark');
+    } else {
+        document.body.classList.remove('dark');
+    }
+    };
+
+    return (
+        <div className="nav-darkmode-toggle-container block px-2">
+            <div className="nav-darkmode-toggle p-2 cursor-pointer" onClick={toggleDarkMode}>
+              {icon} {text}
+            </div>
+          </div>
+    );
+}
+ 
+export default DarkMode;
